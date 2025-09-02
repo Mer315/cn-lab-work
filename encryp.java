1ffdbf3121f769ec9b493c019b21a32a3498030f
Mer315 <153828460+Mer315@users.noreply.github.com>
2025-09-03 00:35:06 +0530

    encryp-decryp added :<

---
+++ encryp.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class encryp {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String text =s.nextLine();
+        int shift =s.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(char c:text.toCharArray()){
+            if(Character.isUpperCase(c)){
+                sb.append((char)((c-'A'+shift)%26+'A'));
+            }
+            else if(Character.isLowerCase(c)){
+                sb.append((char)((c-'a'+shift)%26+'a'));
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+}
